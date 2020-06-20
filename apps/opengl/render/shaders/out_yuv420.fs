a1e5e43d458bad474ac82828917a7177501612da
Luigi Cruz <luigifcruz@gmail.com>
2020-06-20 01:11:51 -0300

    Update output shader.

--- apps/opengl/render/shaders/out_yuv420.fs
+++ apps/opengl/render/shaders/out_yuv420.fs
@@ -1,17 +1,19 @@
-#version 330 core
+#version 300 es
 
 precision highp float;
 
 in vec2 TexCoord;
 uniform sampler2D renderedTexture;
-
-out float FragColor0;
-out float FragColor1;
-out float FragColor2;
+uniform int PlaneId;
+out float FragColor;
 
 void main() {
-    vec3 c = texture(renderedTexture, TexCoord).rgb; 
-    FragColor0 =  (0.257 * c.r) + (0.504 * c.g) + (0.098 * c.b) + 0.0625;
-    FragColor1 =  (0.439 * c.r) - (0.368 * c.g) - (0.071 * c.b) + 0.5000;
-    FragColor2 = -(0.148 * c.r) - (0.291 * c.g) + (0.439 * c.b) + 0.5000;
+    vec3 c = texture(renderedTexture, TexCoord).rgb;
+
+    if (PlaneId == 0) // Y
+        FragColor =  (0.257 * c.r) + (0.504 * c.g) + (0.098 * c.b) + 0.0625;
+    if (PlaneId == 1) // U
+        FragColor = -(0.148 * c.r) - (0.291 * c.g) + (0.439 * c.b) + 0.5000;
+    if (PlaneId == 2) // V
+        FragColor =  (0.439 * c.r) - (0.368 * c.g) - (0.071 * c.b) + 0.5000;
 } 
