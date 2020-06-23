0aa1c34ed7aa224afd60d5dc67867e501d87793e
Luigi Cruz <luigifcruz@gmail.com>
2020-06-23 14:38:17 -0300

    Add math dependency.

--- cmake/glfw.cmake
+++ cmake/glfw.cmake
@@ -5,6 +5,7 @@ add_library(glfw INTERFACE IMPORTED GLOBAL)
 if(LINUX OR MACOS)
     find_package(PkgConfig REQUIRED)
     pkg_check_modules(glfw REQUIRED IMPORTED_TARGET glfw3)
+    list(APPEND glfw_LDFLAGS "-lm")
 endif()
 
 if(WINDOWS)
