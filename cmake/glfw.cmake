1f1415ecdb66eea91c5111738273acaa7323e41b
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2020-06-23 04:42:35 -0700

    solving GLWF issues on linux

--- cmake/glfw.cmake
+++ cmake/glfw.cmake
@@ -1,18 +1,18 @@
-project(glfw)
+# project(glfw)
 
-add_library(glfw INTERFACE IMPORTED GLOBAL)
+# add_library(glfw INTERFACE IMPORTED GLOBAL)
 
-if(LINUX OR MACOS)
-    find_package(PkgConfig REQUIRED)
-    pkg_check_modules(glfw REQUIRED IMPORTED_TARGET glfw3)
-endif()
+# if(LINUX OR MACOS)
+#     find_package(PkgConfig REQUIRED)
+#     pkg_check_modules(glfw REQUIRED IMPORTED_TARGET glfw3)
+# endif()
 
-if(WINDOWS)
-    file(GLOB glfw_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/deps/glfw*/include/")
-    file(GLOB glfw_LDFLAGS "${CMAKE_SOURCE_DIR}/deps/glfw*/lib-vc2019/glfw3dll.lib")
-endif()
+# if(WINDOWS)
+#     file(GLOB glfw_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/deps/glfw*/include/")
+#     file(GLOB glfw_LDFLAGS "${CMAKE_SOURCE_DIR}/deps/glfw*/lib-vc2019/glfw3dll.lib")
+# endif()
 
-target_include_directories(glfw INTERFACE ${glfw_INCLUDE_DIRS})
-target_link_libraries(glfw INTERFACE ${glfw_LDFLAGS})
+# target_include_directories(glfw INTERFACE ${glfw_INCLUDE_DIRS})
+# target_link_libraries(glfw INTERFACE ${glfw_LDFLAGS})
 
-set_property(GLOBAL APPEND PROPERTY GLOBAL_LIB_LIST glfw)
+# set_property(GLOBAL APPEND PROPERTY GLOBAL_LIB_LIST glfw)
