d3c671100282167677d3ae6db63fd6022c0e2679
Luigi Cruz <luigifcruz@gmail.com>
2020-07-06 02:03:50 -0300

    further programming swapchain

--- apps/vulkan/tunnel.cc
+++ apps/vulkan/tunnel.cc
@@ -30,6 +30,12 @@ struct QueueFamilyIndices {
     }
 };
 
+struct SwapChainSupportDetails {
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+};
+
 class VulkanRender {
     public:
         bool start() {
@@ -167,10 +173,42 @@ class VulkanRender {
             return true;
         }
 
+        SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device) {
+            SwapChainSupportDetails details;
+
+            vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+
+            uint32_t formatCount;
+            vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, NULL);
+
+            if (formatCount != 0) {
+                details.formats.resize(formatCount);
+                vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+            }
+
+            uint32_t presentModeCount;
+            vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, NULL);
+
+            if (presentModeCount != 0) {
+                details.presentModes.resize(presentModeCount);
+                vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+            }
+
+            return details;
+        }
+
         bool isDeviceSutable(VkPhysicalDevice device) {
             QueueFamilyIndices indices = findQueueFamilies(device);
+
             bool extensionsSupported = checkDeviceExtensionSupport(device);
-            return indices.isComplete() && extensionsSupported;
+
+            bool swapChainAdequate = false;
+            if (extensionsSupported) {
+                SwapChainSupportDetails swapChainSupport = querySwapChainSupport(device);
+                swapChainAdequate = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
+            }
+
+            return indices.isComplete() && extensionsSupported && swapChainAdequate;
         }
 
         bool createLogicalDevice() {
